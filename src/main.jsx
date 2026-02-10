30f8765099458ce23a3f4254ad4bb075355ca62c
MikkelRuby <eaa23miru@students.eaaa.dk>
2026-02-10 13:16:31 +0100

    Fix basename logic in main.jsx to use correct environment variable for production

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 // Set the basename according to the environment
-const basename = import.meta.env.MODE === "production" ? "/portfolio" : "/";
+const basename = import.meta.env.PROD ? "/portfolio" : "/";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
